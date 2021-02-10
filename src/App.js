1ae4a8dc57db4a55600b968876b48dab4f16ddb0
Kurt Price <kurtlprice@gmail.com>
2021-02-09 20:18:35 -0800

    Add basic home and settings router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
-import HelloWorld from './components/HelloWorld'
-import CounterExample from './components/CounterExample'
+import React from'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
     <div >
         <Header />
+
+        <Router>
+          <ul>
+            <li>
+              <Link to="/" className="text-blue-500"></Link>
+            </li>
+            <li>
+              <Link to="/settings" className="text-blue-500"></Link>
+            </li>
+          </ul>
+        </Router>
+
         <Footer />
     </div>
   );
