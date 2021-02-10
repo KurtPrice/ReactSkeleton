f83c69334f037653cb296753228c222410f14051
Kurt Price <kurtlprice@gmail.com>
2021-02-09 20:01:48 -0800

    Correct ) placement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,9 +18,10 @@ function Navigation() {
     })
 
     const transitions = useTransition(showMenu, null, {
-        from: { position: 'absolute', opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
+        from: { position: 'absolute', opacity: 0  },
+        enter: { opacity: 1  },
+        leave: { opacity: 0  },
+
     })
 
     //className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
@@ -43,6 +44,7 @@ function Navigation() {
                     style={props}>
                     ✌️
                 </animated.div>
+                )
             }
 
         </nav>
