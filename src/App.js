e6f49e92c3f91877dd64a6bf633dac837a308fa6
Kurt Price <kurtlprice@gmail.com>
2021-02-09 22:22:37 -0800

    Add text to links

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ function App() {
           </ul>
         </Router>
 
+        <Switch>
+          <Route exact path="/">
+            <h2>This is the App page</h2>
+          </Route>
+          <Route path="/settings">
+            <h2>This is the settings page</h2>
+          </Route>
+        </Switch>
+
         <Footer />
     </div>
   );
